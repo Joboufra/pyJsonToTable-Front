9cd65687268beb03e9b0dd8c67a9fe2814604a99
Joboufra <joboufra@gmail.com>
2023-11-27 15:54:07 +0100

    fix: header layout in mobile view

--- src/app/components/Search.jsx
+++ src/app/components/Search.jsx
@@ -9,7 +9,7 @@ export default function Search({ onSearch }) {
     <div className="p-4">
       <input
         type="text"
-        className="p-2 md:w-96 w-54 border border-slate-500 bg-slate-800 text-slate-300 text-center rounded"
+        className="p-2 md:w-96 w-40 border border-slate-500 bg-slate-800 text-slate-300 text-center rounded"
         placeholder="Buscar en la tabla..."
         onKeyDown={handleKeyDown}
       />
